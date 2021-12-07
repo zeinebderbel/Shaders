37e8c81065fd748d9201e99ecbdeb88d2b6a9372
zeinebderbel <zeinebdrb@gmail.com>
2021-12-07 16:39:25 +0100

    try texture not finished

--- Ressources/shaders/shader.frag
+++ Ressources/shaders/shader.frag
@@ -1,7 +1,30 @@
 #version 450 core
-out vec4 FragColor;
+
+in vec3 color_out;
+in vec2 uv_out;
+in vec3 normal_out;
+
+out vec4 color;
+
+uniform float scale_frag;
+uniform int width;
+uniform int height;
+uniform int mode;
 
 void main()
 {
-    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-} 
+	//color = vec4(abs(scale_frag)*(gl_FragCoord.x/width), abs(scale_frag)*(gl_FragCoord.y/height), abs(scale_frag)*1,1);
+	if (mode == 0) {
+		color = vec4(color_out, 1);
+	}
+	else if (mode == 1) {
+		color = vec4(normal_out, 1);
+	}
+	else if (mode == 2) {
+		color = vec4(uv_out, 1, 1);
+	}
+	else {
+		color = vec4(abs(scale_frag) * (gl_FragCoord.x / width), abs(scale_frag) * (gl_FragCoord.y / height), abs(scale_frag) * 1, 1);
+	}
+	//vec4(uv_out, 1, 1);//vec4(normal_out,1);//vec4(color_out, 1);
+}
--- Shader_NewSolution/Program.cpp
+++ Shader_NewSolution/Program.cpp
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <string>
 #include "loadShader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 //#ifdef WIN32
 //This magic line is to force notebook computer that share NVidia and Intel graphics to use high performance GPU (NVidia).
@@ -49,12 +51,17 @@ static void GLAPIENTRY GLMessageCallback(GLenum source, GLenum type, GLuint id, 
 
 struct vertex_struct {
 	GLfloat positions[3];
-	GLfloat color[2];
+	GLfloat color[3];
 	GLfloat uv[2];
 	GLfloat normal[3];
 };
 
-
+static const std::vector<vertex_struct> vertices{
+	{{1.0f, -1.0f, 1.0f}, {1.0f,0.0f,0.0f}, {0.0f,0.0f}, {0.0f,0.0f,-1.0f}},
+	{{1.0f, 1.0f, 1.0f},  {0.0f,1.0f,0.0f}, {1.0f,0.0f}, {0.0f,0.0f,-1.0f}},
+	{{-1.0f, -1.0f, 1.0f}, {0.0f,0.0f,1.0f}, {0.0f,1.0f}, {0.0f,0.0f,-1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f,0.0f,0.0f}, {1.0f,1.0f}, {0.0f,0.0f,-1.0f}}
+};
 static void error_callback(int /*error*/, const char* description)
 {
 	std::cerr << "Error: " << description << std::endl;
@@ -104,41 +111,91 @@ int main(void)
 	glDebugMessageCallback(GLMessageCallback, 0);
 	std::cout << "OpenGL debug enabled" << std::endl;
 
-	GLfloat positions[] = { 
-		1.0f, -1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		0.0f, -1.0f, 1.0f
+	unsigned int indices[]
+	{
+		1,0,2,
+		3,2,1
 	};
 	// Buffers
 	GLuint vbo, vao, ebo;
 	glGenBuffers(1, &vbo);
 	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &ebo);
 
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-	std::cout << sizeof(positions) << std::endl;
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_struct)*vertices.size(), vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	std::cout << sizeof(vertex_struct) << std::endl;
 	
+	const auto programId = LoadShaders("../Ressources/shaders/shader.vert", "../Ressources/shaders/shader.frag");
+
+
+	GLint positionLocation = glGetAttribLocation(programId, "vertexPosition_modelspace");
 	glVertexAttribPointer(
-		0,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+		positionLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
 		3,					// size
 		GL_FLOAT,			// type
 		GL_FALSE,			// normalized?
-		sizeof(positions)/(_countof(positions)/3),
-		(GLvoid*)0					// array buffer offset
+		sizeof(vertex_struct),					// stride
+		(void*)offsetof(vertex_struct, vertex_struct::positions)			// array buffer offset
+	);
+	glEnableVertexAttribArray(positionLocation);
+
+	//EnableAttrib<vertex_struct>("vertexPosition_modelspace", programID, 3);
+
+	GLint colorLocation = glGetAttribLocation(programId, "color");
+	glVertexAttribPointer(
+		colorLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,					// size
+		GL_FLOAT,			// type
+		GL_FALSE,			// normalized?
+		sizeof(vertex_struct),					// stride
+		(void*)offsetof(vertex_struct, vertex_struct::color)			// array buffer offset
+	);
+	glEnableVertexAttribArray(colorLocation);
+
+	GLint uvLocation = glGetAttribLocation(programId, "uv");
+	glVertexAttribPointer(
+		uvLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+		2,					// size
+		GL_FLOAT,			// type
+		GL_FALSE,			// normalized?
+		sizeof(vertex_struct),					// stride
+		(void*)offsetof(vertex_struct, vertex_struct::uv)			// array buffer offset
+	);
+	glEnableVertexAttribArray(uvLocation);
+
+	GLint normalLocation = glGetAttribLocation(programId, "normal");
+	glVertexAttribPointer(
+		normalLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,					// size
+		GL_FLOAT,			// type
+		GL_FALSE,			// normalized?
+		sizeof(vertex_struct),					// stride
+		(void*)offsetof(vertex_struct, vertex_struct::normal)			// array buffer offset
 	);
 
-	const auto programId = LoadShaders("../Ressources/shaders/shader.vert", "../Ressources/shaders/shader.frag");
-
-	const auto index = glGetAttribLocation(programId, "aPos");
-
-	/*glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, sizeof(positions), nullptr);*/
-	glEnableVertexAttribArray(index);
-
+	//texture 
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("..\Ressource\tex.jpg", &width, &height, &nrChannels, 0);
+	unsigned int texture;
+	glGenTextures(1, &texture);
 	// Callbacks
 	//glDebugMessageCallback(opengl_error_callback, nullptr);
 	glUseProgram(programId);
+
+	double t;
+
+	GLuint scale_uniform_location = glGetUniformLocation(programId, "scale");
+	GLuint scale_frag_uniform_location = glGetUniformLocation(programId, "scale_frag");
+	GLuint width_uniform_location = glGetUniformLocation(programId, "width");
+	GLuint height_uniform_location = glGetUniformLocation(programId, "height");
+	GLuint mode_location = glGetUniformLocation(programId, "mode");
+	int mode = 0;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -146,13 +203,37 @@ int main(void)
 
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glViewport(0, 0, width, height);
 
+		t = glfwGetTime();
+		float scale_uniform = std::sin(t);
+
+		if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+			mode = 0;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+			mode = 1;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+			mode = 2;
+		}
+		else {
+			mode = -1;
+		}
+
+		glUniform1f(scale_uniform_location, scale_uniform);
+		glUniform1f(scale_frag_uniform_location, scale_uniform);
+		glUniform1i(width_uniform_location, width);
+		glUniform1i(height_uniform_location, height);
+		glUniform1i(mode_location, mode);
+
 		//Loop body // Draw here
 		glBindVertexArray(vao);		
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		//glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 		glBindVertexArray(0);
